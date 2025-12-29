d8a79132ae29b70c0588f2301c1d799c88c40d9d
Hamiltonianini <nuhednoi555@gmail.com>
2025-12-29 23:40:35 +0700

    Lab 11 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,50 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i=0; i<3; i++){
+        cin.get();
+    }
+    srand(time(0));
+    int x = rand()%9+1;
+    string actualGrade;
+
+    switch(x){
+        case 1:
+            actualGrade = "W";
+            break;
+        case 2:
+            actualGrade = "A";
+            break;
+        case 3:
+            actualGrade = "B+";
+            break;
+        case 4:
+            actualGrade = "B";
+            break;
+        case 5:
+            actualGrade = "C+";
+            break;
+        case 6:
+            actualGrade = "C";
+            break;
+        case 7:
+            actualGrade = "D+";
+            break;
+        case 8:
+            actualGrade = "D";
+            break;
+        case 9:
+            actualGrade = "F";
+            break;
+    }
+    cout << "You will get " << actualGrade << " in this 261102.";
+
+}
+
+
+
+
