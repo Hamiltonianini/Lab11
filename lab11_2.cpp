1c0908fcfc5ac856ad719f44ac9be72969d64346
Hamiltonianini <nuhednoi555@gmail.com>
2025-12-30 14:12:38 +0700

    Lab11.2 completed (2)

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -11,6 +11,107 @@ int drawCard(void){
 }
 
 int calScore(int x,int y,int z){
+    int scoreX = cardScores[x];
+    int scoreY = cardScores[y];
+    int scoreZ = cardScores[z];
+    return (scoreX + scoreY + scoreZ) % 10;
+ 
+}
+int findYugiAction(int s){	
+	if(s == 9) return 2; 
+	else if(s < 6) return 1;
+	else if(s>=6 && s<=8){
+	        int r = rand()%100;
+	        if(r<69){
+	            return 1;
+	        }else{
+	            return 2;
+	        }
+	     }
+	return 0; 
+}
+
+void checkWinner(int p, int y){
+	cout << "\n---------------------------------\n";
+	if(p==y){
+	cout <<   "|             Draw!!!           |"; 
+	}else if(p>y){
+	cout <<   "|         Player wins!!!        |"; 
+	}else{
+	cout <<   "|          Yugi wins!!!         |"; 
+	}
+	cout << "\n---------------------------------\n";
+}
+
+int main(){	
+    srand(time(0));
+
+	int playerScore, yugiScore, playerAction, yugiAction;
+	int playerCards[3] = {drawCard(), drawCard(), 0};
+	int yugiCards[3] = {drawCard(), drawCard(), 0};
+	
+	
+	cout << "---------ORE NO TURN, DRAW!!!------------\n";
+	cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
+	playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+	cout << "Your score: " << playerScore << "\n";
+	do{
+		cout << "(1) Destiny draw (2) Stay, SELECT: ";
+		cin >> playerAction;
+	}while(playerAction!=1 && playerAction!=2);
+
+		
+	if(playerAction == 1){
+		//The following lines of code are not completed. Please correct it.
+		playerCards[2]=drawCard();
+		playerScore = calScore(playerCards[0], playerCards[1], playerCards[2]);
+		cout << "Player draws the 3rd card!!!" << "\n";
+		cout << "Your 3rd card: " << cardNames[playerCards[2]] << "\n";
+		cout << "Your new score: " << playerScore << "\n";
+		
+	}
+	cout << "------------ Turn end -------------------\n\n";
+	
+	
+	//The following lines of code for Yugi's turn are not completed. Please correct it.
+	
+	cout << "---------YUGI'S TURN, DRAW!!!------------\n";
+	cout << "Yugi's cards: " << cardNames[yugiCards[0]] << " " << cardNames[yugiCards[1]] << "\n";
+	yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
+	cout << "Yugi's score: " << yugiScore << "\n";
+	yugiAction = findYugiAction(yugiScore);
+
+	
+	if(yugiAction == 1){
+		yugiCards[2] = drawCard();
+		yugiScore = calScore(yugiCards[0], yugiCards[1], yugiCards[2]);
+		cout << "Yugi draws the 3rd card!!!\n";
+		
+		cout << "Yugi's 3rd card: " << cardNames[yugiCards[2]] << "\n";
+		
+		cout << "Yugi's new score: " << yugiScore << "\n";
+	}
+	cout << "------------ Turn end -------------------\n";
+	
+	
+	checkWinner(playerScore,yugiScore);
+}
+
+
+/*
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string cardNames[] = {"","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+int cardScores[] = {0,1,2,3,4,5,6,7,8,9,10,10,10,10};
+
+int drawCard(void){
+    return rand()%13+1;
+}
+
+int calScore(int x,int y,int z){
     int sum;
     if(x<10 && y<10 && z<10){
         sum = x+y+z;
@@ -30,7 +131,6 @@ int calScore(int x,int y,int z){
     return sum%10;
  
 }
-
 int findYugiAction(int s){	
 	if(s == 9) return 2; 
 	else if(s < 6) return 1;
@@ -97,9 +197,9 @@ int main(){
 	yugiAction = findYugiAction(yugiScore);
 	if(yugiAction == 1){
 		cout << "Yugi draws the 3rd card!!!\n";
-		int j = drawCard();
-		yugiScore = calScore(yugiCards[0],yugiCards[1],j);
+		int j = rand()%13+1;
 		cout << "Yugi's 3rd card: " << cardNames[j] << "\n";
+		yugiScore = calScore(yugiCards[0],yugiCards[1],j);
 		cout << "Yugi's new score: " << yugiScore << "\n";
 	}
 	cout << "------------ Turn end -------------------\n";
@@ -108,3 +208,4 @@ int main(){
 	checkWinner(playerScore,yugiScore);
 }
 
+*/
